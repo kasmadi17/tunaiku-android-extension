d095b174e6327516e1d7644e1cc8cc3fa7c44045
Suyanwar <Suyanwar.wawang@gmail.com>
2022-03-24 10:17:49 +0700

    refactor(context-extension): Reformat code

--- extensions/src/main/java/com/tunaikumobile/extensions/ContextExtensions.kt
+++ extensions/src/main/java/com/tunaikumobile/extensions/ContextExtensions.kt
@@ -55,7 +55,8 @@ fun Context.isRooted(): Boolean {
  */
 
 fun Context.isNetworkConnected(): Boolean {
-    val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val connectivityManager =
+        this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     val activeNetworkInfo = connectivityManager.activeNetworkInfo
     return activeNetworkInfo != null && activeNetworkInfo.isConnected
 }
